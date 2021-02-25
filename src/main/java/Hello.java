7c5f801225e8288ec3fe376979e417227bddbf64
muserr888 <66439400+muserr888@users.noreply.github.com>
2021-02-25 14:50:48 +0000

    movie2 branch watching on the phone for the `10` minutes

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World B22");
         System.out.println("B21 future sdet learning git");
+        //movie on the phone 10 minutes
     }
 }
